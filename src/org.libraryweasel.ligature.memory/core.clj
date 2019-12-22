7e89e72f02bbdf8cce8ad8267d0b2b39f1971450
Alex Michael Berry <berry3@indiana.edu>
2019-12-22 00:28:59 -0500

    starting to fill in Clojure impl, #4

--- src/org.libraryweasel.ligature.memory/core.clj
+++ src/org.libraryweasel.ligature.memory/core.clj
@@ -8,7 +8,62 @@
 
 (ns org.libraryweasel.ligature.memory.core)
 
-(defn create-ligature-in-memory
-  ""
+(defn ligature-memory-store
+  "Creates an in-memory implementation of the LigatureStore protocol."
   []
-  nil)
+  (let [datasets (ref {})]
+    (reify LigatureStore
+      (get-dataset
+        [this dateset-name]
+        (if (contains? @datasets dataset-name) ;; TODO this should be in a transaction
+          (dataset-name @datasets)
+          (comment create a new dataset and add to the datasets map)))
+      (delete-dataset
+        [this dateset-name]
+        (comment remove dataset-name))
+      (all-datasets
+        [this]
+        (keys datasets))
+      (close
+        [this]
+        (comment "do nothing"))
+      (location
+        [this]
+        "memory"))))
+
+(defn ligature-memory-dataset
+  "Creates an in-memory implementation of the LigatureDataset protocol."
+  [name]
+  (let
+    [spog (ref {})]
+    (reify LigatureDataset
+      (add-statements
+        [this statements]
+        )
+      (remove-statements
+        [this statements]
+        )
+      (all-statements
+        [this]
+        )
+      (new-identifier
+        [this]
+        )
+      (match
+        [this pattern]
+        )
+      (dataset-name
+        [this]
+        )
+      (set-ontology
+        [this ontology]
+        )
+      (get-ontology
+        [this]
+        )
+      (sparql-query
+        [this query]
+        )
+      (wander-query
+        [this query]
+        ))))
