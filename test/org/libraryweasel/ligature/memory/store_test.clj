3330aef17606a1ad2b6d8d7dfcdee2cdd2359e00
Alex Michael Berry <berry3@indiana.edu>
2020-02-21 08:15:30 -0500

    Updating tests to use `a` instead of `:a`

--- test/org/libraryweasel/ligature/memory/store_test.clj
+++ test/org/libraryweasel/ligature/memory/store_test.clj
@@ -48,52 +48,52 @@
     (let [store (ligature-memory-store)]
       (is (not (= (create-collection store "test") nil))) ;TODO maybe check collection type instead of just making sure it's not null
       (let [tx (writeTx (collection store "test"))]
-        (add-statement tx ["This" :a "test"])
+        (add-statement tx ["This" a "test"])
         (commit tx))
       (let [tx (readTx (collection store "test"))]
-        (is (= (set (all-statements tx)) #{["This" :a "test"]}))
+        (is (= (set (all-statements tx)) #{["This" a "test"]}))
         (cancel tx))))
 
   (testing "adding rules to collections"
     (let [store (ligature-memory-store)]
       (is (not (= (create-collection store "test") nil))) ;TODO maybe check collection type instead of just making sure it's not null
       (let [tx (writeTx (collection store "test"))]
-        (add-rule tx ["Also" :a "test"])
+        (add-rule tx ["Also" a "test"])
         (commit tx))
       (let [tx (readTx (collection store "test"))]
-        (is (= (set (all-rules tx)) #{["Also" :a "test"]}))
+        (is (= (set (all-rules tx)) #{["Also" a "test"]}))
         (cancel tx))))
 
   (testing "removing statements from collections"
     (let [store (ligature-memory-store)]
       (is (not (= (create-collection store "test") nil))) ;TODO maybe check collection type instead of just making sure it's not null
       (let [tx (writeTx (collection store "test"))]
-        (add-statement tx ["This" :a "test"])
-        (add-statement tx ["Also" :a "test"])
-        (remove-statement tx ["This" :a "test"])
+        (add-statement tx ["This" a "test"])
+        (add-statement tx ["Also" a "test"])
+        (remove-statement tx ["This" a "test"])
         (commit tx))
       (let [tx (readTx (collection store "test"))]
-        (is (= (set (all-statements tx)) #{["Also" :a "test"]}))
+        (is (= (set (all-statements tx)) #{["Also" a "test"]}))
         (cancel tx))))
 
   (testing "removing rules from collections"
     (let [store (ligature-memory-store)]
       (is (not (= (create-collection store "test") nil))) ;TODO maybe check collection type instead of just making sure it's not null
       (let [tx (writeTx (collection store "test"))]
-        (add-rule tx ["This" :a "test"])
-        (add-rule tx ["Also" :a "test"])
-        (remove-rule tx ["This" :a "test"])
+        (add-rule tx ["This" a "test"])
+        (add-rule tx ["Also" a "test"])
+        (remove-rule tx ["This" a "test"])
         (commit tx))
       (let [tx (readTx (collection store "test"))]
-        (is (= (set (all-rules tx)) #{["Also" :a "test"]}))
+        (is (= (set (all-rules tx)) #{["Also" a "test"]}))
         (cancel tx))))
 
   (testing "matching statements in collections"
     (let [store (ligature-memory-store)]
       (is (not (= (create-collection store "test") nil))) ;TODO maybe check collection type instead of just making sure it's not null
       (let [tx (writeTx (collection store "test"))]
-        (add-statement tx ["This" :a "test"])
-        (add-statement tx [(new-identifier tx) :a "test"])
+        (add-statement tx ["This" a "test"])
+        (add-statement tx [(new-identifier tx) a "test"])
         (add-statement tx ["a" "knows" "b"])
         (add-statement tx ["b" "knows" "c"])
         (add-statement tx ["c" "knows" "a"])
@@ -102,9 +102,9 @@
         (commit tx))
       (let [tx (readTx (collection store "test"))]
         (is (= (count (match-statements tx [:? :? :?])) 6))
-        (is (= (set (match-statements tx [:? :a :?])) #{["This" :a "test"] ["Also" :a "test"]}))
-        (is (= (set (match-statements tx [:? :a "test"])) #{["This" :a "test"] ["Also" :a "test"]}))
-        (is (= (set (match-statements tx [:? :? "test"])) #{["This" :a "test"] ["Also" :a "test"]}))
+        (is (= (set (match-statements tx [:? a :?])) #{["This" a "test"] ["Also" a "test"]}))
+        (is (= (set (match-statements tx [:? a "test"])) #{["This" a "test"] ["Also" a "test"]}))
+        (is (= (set (match-statements tx [:? :? "test"])) #{["This" a "test"] ["Also" a "test"]}))
         (is (= (set (match-statements tx [:? :? :? ":_5"])) #{["_:2" "_:3" "_:4" "_:5"]}))
         (cancel tx))))
 
@@ -112,8 +112,8 @@
     (let [store (ligature-memory-store)]
       (is (not (= (create-collection store "test") nil))) ;TODO maybe check collection type instead of just making sure it's not null
       (let [tx (writeTx (collection store "test"))]
-        (add-rule tx ["This" :a "test"])
-        (add-rule tx [(new-identifier tx) :a "test"])
+        (add-rule tx ["This" a "test"])
+        (add-rule tx [(new-identifier tx) a "test"])
         (add-rule tx ["a" "knows" "b"])
         (add-rule tx ["b" "knows" "c"])
         (add-rule tx ["c" "knows" "a"])
@@ -122,8 +122,8 @@
         (commit tx))
       (let [tx (readTx (collection store "test"))]
         (is (= (count (match-rules tx [:? :? :?])) 6))
-        (is (= (set (match-rules tx [:? :a :?])) #{["This" :a "test"] ["Also" :a "test"]}))
-        (is (= (set (match-rules tx [:? :a "test"])) #{["This" :a "test"] ["Also" :a "test"]}))
-        (is (= (set (match-rules tx [:? :? "test"])) #{["This" :a "test"] ["Also" :a "test"]}))
+        (is (= (set (match-rules tx [:? a :?])) #{["This" a "test"] ["Also" a "test"]}))
+        (is (= (set (match-rules tx [:? a "test"])) #{["This" a "test"] ["Also" a "test"]}))
+        (is (= (set (match-rules tx [:? :? "test"])) #{["This" a "test"] ["Also" a "test"]}))
         (is (= (set (match-rules tx [:? :? :? ":_5"])) #{["_:2" "_:3" "_:4" "_:5"]}))
         (cancel tx)))))
