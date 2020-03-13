63558b22c3325ba765ed969d3e18b8b7ea84b457
Alex Michael Berry <berry3@indiana.edu>
2020-03-13 00:08:43 -0400

    removing project.clj

--- project.clj
+++
@@ -1,7 +0,0 @@
-(defproject ligature-in-memory "0.1.0-SNAPSHOT"
-  :description "An In-Memory implementation of Ligature."
-  :url "https://github.com/almibe/ligature-in-memory"
-  :license {:name "EPL-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"], [org.libraryweasel/ligature "0.1.0-SNAPSHOT"]]
-  :repl-options {:init-ns org.libraryweasel.ligature.memory.store})
