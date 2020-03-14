913be3711616aa335e51441192080c324e0ba21e
Alex Michael Berry <berry3@indiana.edu>
2020-03-14 19:23:33 -0400

    starting to update tests, #28

--- src/test/kotlin/org/libraryweasel/ligature/memory/LibraryTest.kt
+++ src/test/kotlin/org/libraryweasel/ligature/memory/LibraryTest.kt
@@ -4,48 +4,50 @@
 
 package org.libraryweasel.ligature.memory
 
+import io.kotlintest.shouldNotBe
 import io.kotlintest.specs.StringSpec
+import kotlinx.coroutines.flow.toList
+import org.libraryweasel.ligature.Entity
 
 class InMemorySpec: StringSpec({
-//    (ns org.libraryweasel.ligature.memory.store-test
-//    (:require [clojure.test :refer :all]
-//    [org.libraryweasel.ligature.memory.store :refer :all]
-//    [org.libraryweasel.ligature.core :refer :all]))
-//
-//    (deftest store-test
-//    (testing "Create and close store"
-//    (let [store (ligature-memory-store)]
-//    (is (= (details store) {:location "memory"}))
-//    (is (= (all-collections store) #{}))
-//    (close store)))
-//
-//    (testing "access new collection"
-//    (let [store (ligature-memory-store)]
-//    (is (not (= (collection store "test") nil))) ;TODO maybe check collection type instead of just making sure it's not null
-//    (is (= (all-collections store) #{}))))
-//
-//    (testing "creating a new collection"
-//    (let [store (ligature-memory-store)]
-//    (is (not (= (create-collection store "test") nil))) ;TODO maybe check collection type instead of just making sure it's not null
-//    (is (= (all-collections store) #{"test"}))))
-//
-//    (testing "access and delete new collection"
+    "Create and close store" {
+        val store = InMemoryStore()
+        store.details() shouldBe mapOf("location" to "memory")
+        store.allCollections().toList() sholdBe listOf<Entity>()
+        store.close()
+    }
+
+    "access new collection" {
+        val store = InMemoryStore()
+        store.collection(Entity("test")) shouldNotBe  null
+        store.allCollections().toList() shouldBe listOf<Entity>()
+    }
+
+    "creating a new collection" {
+        val store = InMemoryStore()
+        store.createCollection(Entity("test")) shouldNotBe  null
+        store.allCollections().toList() shouldBe listOf<Entity>(Entity("test"))
+    }
+
+    "access and delete new collection" {
 //    (let [store (ligature-memory-store)]
 //    (is (not (= (create-collection store "test") nil))) ;TODO maybe check collection type instead of just making sure it's not null
 //    (is (= (all-collections store) #{"test"}))
 //    (delete-collection store "test")
 //    (delete-collection store "test2")
 //    (is (= (all-collections store) #{}))))
-//
-//    (testing "new collections should be empty"
+    }
+
+    "new collections should be empty" {
 //    (let [store (ligature-memory-store)]
 //    (is (not (= (create-collection store "test") nil))) ;TODO maybe check collection type instead of just making sure it's not null
 //    (let [tx (readTx (collection store "test"))]
 //    (is (= (set (all-statements tx)) #{}))
 //    (is (= (set (all-rules tx)) #{}))
 //    (cancel tx))))
-//
-//    (testing "adding statements to collections"
+    }
+
+    "adding statements to collections" {
 //    (let [store (ligature-memory-store)]
 //    (is (not (= (create-collection store "test") nil))) ;TODO maybe check collection type instead of just making sure it's not null
 //    (let [tx (writeTx (collection store "test"))]
@@ -54,8 +56,9 @@ class InMemorySpec: StringSpec({
 //    (let [tx (readTx (collection store "test"))]
 //    (is (= (set (all-statements tx)) #{["This" a "test" _]}))
 //    (cancel tx))))
-//
-//    (testing "adding rules to collections"
+    }
+
+    "adding rules to collections" {
 //    (let [store (ligature-memory-store)]
 //    (is (not (= (create-collection store "test") nil))) ;TODO maybe check collection type instead of just making sure it's not null
 //    (let [tx (writeTx (collection store "test"))]
@@ -64,8 +67,9 @@ class InMemorySpec: StringSpec({
 //    (let [tx (readTx (collection store "test"))]
 //    (is (= (set (all-rules tx)) #{["Also" a "test"]}))
 //    (cancel tx))))
-//
-//    (testing "removing statements from collections"
+    }
+
+    "removing statements from collections" {
 //    (let [store (ligature-memory-store)]
 //    (is (not (= (create-collection store "test") nil))) ;TODO maybe check collection type instead of just making sure it's not null
 //    (let [tx (writeTx (collection store "test"))]
@@ -76,8 +80,9 @@ class InMemorySpec: StringSpec({
 //    (let [tx (readTx (collection store "test"))]
 //    (is (= (set (all-statements tx)) #{["Also" a "test" _]}))
 //    (cancel tx))))
-//
-//    (testing "removing rules from collections"
+    }
+
+    "removing rules from collections" {
 //    (let [store (ligature-memory-store)]
 //    (is (not (= (create-collection store "test") nil))) ;TODO maybe check collection type instead of just making sure it's not null
 //    (let [tx (writeTx (collection store "test"))]
@@ -88,8 +93,9 @@ class InMemorySpec: StringSpec({
 //    (let [tx (readTx (collection store "test"))]
 //    (is (= (set (all-rules tx)) #{["Also" a "test"]}))
 //    (cancel tx))))
-//
-//    (testing "matching statements in collections"
+    }
+
+    "matching statements in collections" {
 //    (let [store (ligature-memory-store)]
 //    (is (not (= (create-collection store "test") nil))) ;TODO maybe check collection type instead of just making sure it's not null
 //    (let [tx (writeTx (collection store "test"))]
@@ -109,4 +115,5 @@ class InMemorySpec: StringSpec({
 //    (is (= (set (match-statements tx [:? :? "test" :?])) #{["This" a "test" _] ["_:1" a "test" _]}))
 //    (is (= (set (match-statements tx [:? :? :? "_:5"])) #{["_:2" "_:3" "_:4" "_:5"]}))
 //    (cancel tx))))) ; TODO add test running against a non-existant collection w/ match-statement calls
+    }
 })
