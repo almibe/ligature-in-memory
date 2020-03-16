ecd7aaee50f8b962f5f0b1591174b6b19e26ac5d
Alex Michael Berry <berry3@indiana.edu>
2020-03-15 22:31:42 -0400

    using ligature-test-suite now, closes #6

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.4")
     implementation("io.vavr:vavr:1.0.0-alpha-3")
     implementation("org.libraryweasel.ligature:ligature:0.1.0-SNAPSHOT")
+    testImplementation("org.libraryweasel.ligature:ligature-test-suite:0.1.0-SNAPSHOT")
     testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
 }
 
