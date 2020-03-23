9084400380add337ef344817f0f1b6652ea0ec42
Alex Michael Berry <berry3@indiana.edu>
2020-03-23 02:28:47 -0400

    removing gradle files + starting to add Rust

--- test/test.rs
+++ test/test.rs
@@ -2,9 +2,3 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package org.libraryweasel.ligature.memory
-
-import io.kotlintest.specs.StringSpec
-import org.libraryweasel.ligature.test.createSpec
-
-class InMemorySpec: StringSpec({createSpec { InMemoryStore() }()  })
