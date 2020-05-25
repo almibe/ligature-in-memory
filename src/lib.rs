1681df11adc18103eee2e48aa340e606b1b813f9
Alex Michael Berry <berry3@indiana.edu>
2020-05-24 20:57:45 -0400

    starting to fill in basic impl, #32

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,113 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-use ligature::Statement;
+use ligature::*;
+use im::OrdSet;
+use im::HashMap;
+use futures::Stream;
 
+struct InMemoryStore {
+    store: HashMap<String, OrdSet<Statement>>
+}
+
+impl LigatureStore for InMemoryStore {
+    fn read_tx(&self) -> &dyn ReadTx {
+        unimplemented!()
+    }
+
+    fn write_tx(&self) -> &dyn WriteTx {
+        unimplemented!()
+    }
+
+    fn close(&self) {
+        unimplemented!()
+    }
+
+    fn is_open(&self) -> bool {
+        unimplemented!()
+    }
+}
+
+pub fn ligature_in_memory() -> Box<dyn LigatureStore> {
+    Box::new(InMemoryStore { store: HashMap::new() })
+}
+
+struct InMemoryReadTx {
+
+}
+
+impl ReadTx for InMemoryReadTx {
+    fn collections(&self) -> &dyn Stream<Item=CollectionName> {
+        unimplemented!()
+    }
+
+    fn collections_prefix(&self, prefix: CollectionName) -> &dyn Stream<Item=CollectionName> {
+        unimplemented!()
+    }
+
+    fn collections_range(&self, from: CollectionName, to: CollectionName) -> &dyn Stream<Item=CollectionName> {
+        unimplemented!()
+    }
+
+    fn all_statements(&self, collection: CollectionName) -> &dyn Stream<Item=Statement> {
+        unimplemented!()
+    }
+
+    fn match_statements(&self, collection: CollectionName, subject: Option<Entity>, predicate: Option<Predicate>, object: Option<Object>, context: Option<Entity>) -> &dyn Stream<Item=Statement> {
+        unimplemented!()
+    }
+
+    fn match_statements_range(&self, collection: CollectionName, subject: Option<Entity>, predicate: Option<Predicate>, range: Option<Range>, context: Option<Entity>) -> &dyn Stream<Item=Statement> {
+        unimplemented!()
+    }
+
+    fn cancel(&self) {
+        unimplemented!()
+    }
+
+    fn is_open(&self) -> bool {
+        unimplemented!()
+    }
+}
+
+struct InMemoryWriteTx {
+
+}
+
+impl WriteTx for InMemoryWriteTx {
+    fn create_collection(&self, collection: CollectionName) {
+        unimplemented!()
+    }
+
+    fn delete_collection(&self, collection: CollectionName) {
+        unimplemented!()
+    }
+
+    fn new_entity(&self, collection: CollectionName) -> Entity {
+        unimplemented!()
+    }
+
+    fn remove_entity(&self, collection: CollectionName, entity: Entity) {
+        unimplemented!()
+    }
+
+    fn add_statement(&self, collection: CollectionName, statement: Statement) {
+        unimplemented!()
+    }
+
+    fn remove_statement(&self, collection: CollectionName, statement: Statement) {
+        unimplemented!()
+    }
+
+    fn commit(&self) {
+        unimplemented!()
+    }
+
+    fn cancel(&self) {
+        unimplemented!()
+    }
+
+    fn is_open(&self) -> bool {
+        unimplemented!()
+    }
+}
