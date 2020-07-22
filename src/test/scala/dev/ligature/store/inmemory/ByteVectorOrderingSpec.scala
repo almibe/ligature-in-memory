039d4cd1ccfbfce86bd94fc6a0164b2fd53c6734
Alex Michael Berry <berry3@indiana.edu>
2020-07-22 05:55:51 -0400

    basic support for testing ByteVectorOrdering, closes #45

--- src/test/scala/dev/ligature/store/inmemory/ByteVectorOrderingSpec.scala
+++ src/test/scala/dev/ligature/store/inmemory/ByteVectorOrderingSpec.scala
@@ -4,11 +4,30 @@
 
 package dev.ligature.store.inmemory
 
+import dev.ligature.store.inmemory.InMemoryKeyValueStore.ByteVectorOrdering
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import scodec.bits.ByteVector
+import scodec.codecs.byte
+
+import scala.collection.mutable
 
 class ByteVectorOrderingSpec extends AnyFlatSpec with Matchers {
-  it should "blah" in {
-    1 + 1 shouldBe 2
+  "ByteVectorOrdering" should "order ByteVectors lexicographically" in {
+    val encoder2 = byte ~~ byte
+    val encoder = byte ~~ byte ~~ byte
+
+    val bv1 = encoder.encode(0.toByte, 0.toByte, 0.toByte).require.bytes
+    val bv2 = encoder.encode(0.toByte, 0.toByte, 1.toByte).require.bytes
+    ByteVectorOrdering.compare(bv1, bv2) shouldBe -1
+
+    val bv3 = encoder.encode(0.toByte, 1.toByte, 1.toByte).require.bytes
+    val bv4 = encoder2.encode(0.toByte, 1.toByte).require.bytes
+    ByteVectorOrdering.compare(bv3, bv4) shouldBe 1
+
+    val t = mutable.TreeMap[ByteVector, ByteVector]()(ByteVectorOrdering)
+    t.put(bv1, bv1)
+    t.put(bv2, bv2)
+    t.range(ByteVector.fromByte(0.toByte), ByteVector.fromByte(3.toByte)).size shouldBe 2
   }
 }
